00543aac273db41b85d5c60a67e90e027063bb00
yogi1616 <yogeshbarot07@gmail.com>
2020-04-11 17:46:03 +0530

    updated sign-in-and-sign-up component and finishing signing handle submit

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,34 @@ const config = {
     measurementId: "G-3MBZVF1WZN"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    
+    const snapshot = await userRef.get();
+
+    if(!snapshot.exists) {
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }else{
+
+    }
+    
+    return userRef;
+  }
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
