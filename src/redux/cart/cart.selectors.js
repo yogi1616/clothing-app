b67305c98336961e44f777a202fb069063a61b4e
yogi1616 <yogeshbarot07@gmail.com>
2020-04-18 17:30:24 +0530

    addItem, removeItem function added and add or remove an item from checkout page is done

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -19,5 +19,5 @@ export const selectCartItemsCount = createSelector(
 
 export const selectCartTotal = createSelector(
     [selectCartItems],
-    cartItems => cartItems.reduce((accumalatedQuantity, cartItem) => (accumalatedQuantity + cartItem.quantity) * cartItem.price, 0)
+    cartItems => cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity * cartItem.price, 0)
 )
